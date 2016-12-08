62a8355bfabf7079426348d4563b81e2feba99b0
Pierre <pleveque.pro@gmail.com>
2016-12-08 15:49:08 +0000

    Refactoring code
    New enumerations created with specifities of guitar to avoid errors value by
    users
    
    Guitar class changes attributes : replaced string by enumerations
    
    Search method in Inventory class changes, model enter by user is directly
    changed by Lower case. Code is more simplified and comprehensible and
    avoid errors
    
    Each specs of guitar are added in list. The method return the list of
    guitars

--- src/Inventory.java
+++ src/Inventory.java
@@ -13,49 +13,44 @@ public class Inventory {
         guitars = new LinkedList();
     }
 
-    public void addGuitar(String serialNumber, double price, String builder,
-                          String model, String type, String backWood, String topWood){
-
-        Guitar guitar = new Guitar(serialNumber, price, builder, model, type, backWood, topWood);
+    public void addGuitar(String serialNumber, double price,
+                          Builder builder, String model,
+                          Type type, Wood backWood, Wood topWood) {
+        Guitar guitar = new Guitar(serialNumber, price, builder,
+                model, type, backWood, topWood);
         guitars.add(guitar);
     }
 
-    public Guitar getGuitar(String serialNumber){
-        for(Iterator i = guitars.iterator(); i.hasNext();){
-            Guitar guitar = (Guitar) i.next();
-            if(guitar.getSerialNumber().equals(serialNumber)){
+    public Guitar getGuitar(String serialNumber) {
+        for (Iterator i = guitars.iterator(); i.hasNext(); ) {
+            Guitar guitar = (Guitar)i.next();
+            if (guitar.getSerialNumber().equals(serialNumber)) {
                 return guitar;
             }
         }
         return null;
     }
 
-    public Guitar search(Guitar searchGuitar){
-        for(Iterator i = guitars.iterator(); i.hasNext(); ){
+    public List search(Guitar searchGuitar) {
+        List matchingGuitars = new LinkedList();
+        for (Iterator i = guitars.iterator(); i.hasNext(); ) {
             Guitar guitar = (Guitar)i.next();
-
-            String builder = searchGuitar.getBuilder();
-            if((builder != null) && (!builder.equals("")) &&
-                    (!builder.equals((guitar.getBuilder()))))
+            // Ignore serial number since that's unique
+            // Ignore price since that's unique
+            if (searchGuitar.getBuilder() != guitar.getBuilder())
                 continue;
-            String model = searchGuitar.getModel();
-            if((model != null) && (!builder.equals("")) &&
-                    (!model.equals(guitar.getModel())))
-            continue;
-            String type = searchGuitar.getType();
-            if((type != null) && (!builder.equals("")) &&
-                    (!type.equals(guitar.getType())))
+            String model = searchGuitar.getModel().toLowerCase();
+            if ((model != null) && (!model.equals("")) &&
+                    (!model.equals(guitar.getModel().toLowerCase())))
                 continue;
-            String backWood = searchGuitar.getBackWood();
-            if((backWood != null) && (!builder.equals("")) &&
-                    (!backWood.equals(guitar.getBackWood())))
+            if (searchGuitar.getType() != guitar.getType())
                 continue;
-            String topWood = searchGuitar.getTopWood();
-            if((topWood != null) && (!builder.equals("")) &&
-                    (!topWood.equals(guitar.getTopWood())))
+            if (searchGuitar.getBackWood() != guitar.getBackWood())
                 continue;
+            if (searchGuitar.getTopWood() != guitar.getTopWood())
+                continue;
+            matchingGuitars.add(guitar);
         }
-        return null;
+        return matchingGuitars;
     }
-
 }
