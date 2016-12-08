11054dcaa414250e153a7f9d141d30d6ab961bcd
Pierre <pleveque.pro@gmail.com>
2016-12-08 14:56:16 +0000

    Added constructor, getters/setter in guitar class
    Created attributes which correspond to specifities of one Guitar
    
    Added Constructor which take in arguments all attributes, because one
    guitar is created with theses attributes
    
    Added getters and setter price

--- src/Guitar.java
+++ src/Guitar.java
@@ -1,5 +1,51 @@
 /**
- * Created by Pc_Pierre on 08/12/2016.
+ * Created by Pierre on 08/12/2016.
  */
 public class Guitar {
+
+    private String serialNumber, builder, model, type, backWood, topWood;
+    private double price;
+
+    public Guitar(String serialNumber, double price, String builder,
+                  String model, String type, String backWood, String topWood){
+        this.serialNumber = serialNumber;
+        this.price = price;
+        this.builder = builder;
+        this.model = model;
+        this.type = type;
+        this.backWood = backWood;
+        this.topWood = topWood;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public String getBuilder() {
+        return builder;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getBackWood() {
+        return backWood;
+    }
+
+    public String getTopWood() {
+        return topWood;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
